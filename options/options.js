263f3b3da5f9551dc356b9a869b4fd6fbe22898c
Crowdbotics <team@crowdbotics.com>
2021-09-16 02:24:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royal_dew_30588Identifier",
+  url: "https://royal_dew_30588Identifier.botics.co",
+  api: "https://royal_dew_30588Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
